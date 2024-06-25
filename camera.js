42911df497b994ef36a5b6f88506d8a9c1a506d3
Anton Starostin <anton.starostin@bd.com>
2024-06-26 02:18:48 +0300

    Set initial camera position

--- camera.js
+++ camera.js
@@ -3,8 +3,8 @@ export class Camera {
         this.map = map;
         this.width = width;
         this.height = height;
-        this.x = 128;
-        this.y = 128;
+        this.x = 0;
+        this.y = 0;
         this.maxX = map.cols * map.tileSize - this.width;
         this.maxY = map.rows * map.tileSize - this.height;
         this.speed = 500; //pixels per seconds
